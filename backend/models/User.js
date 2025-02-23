2d68428f8828bcea4b73ab1df7d48880c9978553
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-02-24 00:48:02 +0530

    Schema Design

---
+++ backend/models/User.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isOnline: { type: Boolean, default: false },
+  lastSeen: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', userSchema);
+
+export { User };
